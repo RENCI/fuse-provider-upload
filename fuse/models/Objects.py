7f501e7a359aefdda29ca6a612e4d09493dce3eb
Jason Reilly <jdr0887@gmail.com>
2022-04-22 12:25:31 -0400

    adding in requested_object_id

--- fuse/models/Objects.py
+++ fuse/models/Objects.py
@@ -2,7 +2,7 @@ import inspect
 from enum import Enum
 from typing import Type, List, Optional
 
-from fastapi import Form, Query
+from fastapi import Form
 from pydantic import BaseModel, Field, EmailStr
 
 
@@ -154,5 +154,5 @@ class ProviderParameters(BaseModel):
     aliases: Optional[str] = Field(None, title="Optional list of aliases for this object")
     checksums: Optional[List[Checksums]] = Field(None, title="Optional checksums for the object",
                                                  description="enables verification checking by clients; this is a json list of objects, each object contains 'checksum' and 'type' fields, where 'type' might be 'sha-256' for example.")
-    requested_object_id: str = Query(default=None,
-                                     description="optional argument to be used by submitter to request an object_id; this could be, for example, used to retrieve objects from a 3rd party for which this endpoint is a proxy. The requested object_id is not guaranteed, enduser should check return value for final object_id used."),
+    requested_object_id: Optional[str] = Field(default=None,
+                                               description="optional argument to be used by submitter to request an object_id; this could be, for example, used to retrieve objects from a 3rd party for which this endpoint is a proxy. The requested object_id is not guaranteed, enduser should check return value for final object_id used."),
